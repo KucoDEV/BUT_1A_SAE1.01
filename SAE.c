964b5b8fa1a2889198a19ac4b1aa8607c089332e
PICHOT-MOISE Matheo <Matheo.PICHOT-MOISE@etu.uca.fr>
2024-10-23 08:27:53 +0200

    Ajout lecture fichier ofrestage.txt

--- SAE.c
+++ SAE.c
@@ -1,2 +1,71 @@
 #include "SAE.h"
 
+int remplirOffreStage(int tRef[], int tDpt[], int tPourvu[], int tCandid[], int tEtu1[], int tEtu2[], int tEtu3[], int *tlog, int tmax) {
+    FILE *flot;
+    int ref, dept, pourvu, candid, etu1, etu2, etu3;
+    int i=0;
+
+    flot = fopen("DATA/offrestage.txt", "r"); // Ouvrir le fichier
+    
+    if (flot  == NULL) return -1; // Erreur lors du chargement
+    
+    while(i < tmax && fscanf(flot, "%d", &ref) == 1) { // Si i<tmax et que le fichier trouve encore une ligne
+        if (ref >= 1000) {
+            tRef[i] = ref;
+            if (fscanf(flot, "%d%d%d", &dept, &pourvu, &candid) != 3) { // Si les 3 ne sont pas présent c'est pas une ligne correcte
+                break;
+            }
+
+            if (candid==3) {
+                if (fscanf(flot, "%d%d%d", &etu1, &etu2, &etu3) != 3) {
+                    break;
+                }
+            }
+            else if (candid==2) {
+                if (fscanf(flot, "%d%d", &etu1, &etu2) != 2) {
+                    break;
+                }
+                etu3=0;
+            }
+            else if  (candid==1) {
+                if (fscanf(flot, "%d", &etu1) != 1) {
+                    break;
+                }
+                etu2=0; 
+                etu3=0;
+            }
+            else {
+                etu1=0;
+                etu2=0;
+                etu3=0;
+            }
+            
+            tDpt[i] = dept;
+            tPourvu[i] = pourvu;
+            tCandid[i] = candid;
+            tEtu1[i] = etu1;
+            tEtu2[i] = etu2;
+            tEtu3[i] = etu3;
+
+            i++;
+            (*tlog)++;
+        }
+    }
+    fclose(flot);
+    return 1; // Fonction réussi
+}
+
+void global(void) {
+    int choix, code;
+    // Tableaux Offres De Stages
+    int tRef[200]={0}, tDpt[200]={0}, tPourvu[200]={0}, tCandid[200]={0}, tEtu1[200]={0}, tEtu2[200]={0}, tEtu3[200]={0}, tmaxOffre=200, tlogOffre=0;
+    // Tableaux Liste Etudiants
+    int tNumEtu[200]={0}, tRefStage[200]={0}, tmaxEtu=200, tlogEtu=0;
+    float tNoteFinal[200];
+
+    code = remplirOffreStage(tRef, tDpt, tPourvu, tCandid, tEtu1, tEtu2, tEtu3, &tlogOffre, tmaxOffre); // Remplir le tableau des offres de stages
+    if (code == -1) {
+        printf("\nUne erreur est survenue lors du chargement du fichier (Offre Stage) !\n");
+        exit(1); // Termine tout
+    }
+}
