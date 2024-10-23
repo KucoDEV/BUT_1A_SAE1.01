847160aa1018a2bcf52c11c163a51e56ac0ed3a3
Quentin MIOTTO <Quentin.MIOTTO@etu.uca.fr>
2024-10-23 08:51:44 +0200

    Modification .h

--- SAE.h
+++ SAE.h
@@ -1,2 +1,9 @@
 #include <stdio.h>
 
+// Remplir Tableaux
+int remplirOffreStage(int tRef[], int tDpt[], int tPourvu[], int tCandid[], int tEtu1[], int tEtu2[], int tEtu3[], int *tlog, int tmax);
+int remplirListeEtudiants(int tNumEtu[], int tRefStage[], float tNoteFInal[], int *tlog, int tmax);
+
+// Global
+int menuSelection(void);
+void global(void);
