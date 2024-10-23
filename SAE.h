92426ec0aabfc3be25ba4f76c11704c752137213
Matheo PICHOT-MOISE <Matheo.PICHOT-MOISE@etu.uca.fr>
2024-10-23 09:05:56 +0200

    Modification .h

--- SAE.h
+++ SAE.h
@@ -4,6 +4,17 @@
 int remplirOffreStage(int tRef[], int tDpt[], int tPourvu[], int tCandid[], int tEtu1[], int tEtu2[], int tEtu3[], int *tlog, int tmax);
 int remplirListeEtudiants(int tNumEtu[], int tRefStage[], float tNoteFInal[], int *tlog, int tmax);
 
+// Responsable
+int afficherTableau(int tRef[], int tDpt[], int tPourvu[], int tCandid[], int tEtu1[], int tEtu2[], int tEtu3[], int *tlog, int tmax);
+int menuResponsable(void);
+int globalResponsable(int tRef[], int tDpt[], int tPourvu[], int tCandid[], int tEtu1[], int tEtu2[], int tEtu3[], int tNumEtu[], int tRefStage[], float tNoteFInal[], int *tlogOffre, int tmaxOffre, int *tlogEtu, int tmaxEtu);
+
+// Etudiant
+int globalEtudiant(int tRef[], int tDpt[], int tPourvu[], int tCandid[], int tEtu1[], int tEtu2[], int tEtu3[], int tNumEtu[], int tRefStage[], float tNoteFInal[], int *tlogOffre, int tmaxOffre, int *tlogEtu, int tmaxEtu);
+
+// Jury
+int globalJury(int tRef[], int tDpt[], int tPourvu[], int tCandid[], int tEtu1[], int tEtu2[], int tEtu3[], int tNumEtu[], int tRefStage[], float tNoteFInal[], int *tlogOffre, int tmaxOffre, int *tlogEtu, int tmaxEtu);
+
 // Global
 int menuSelection(void);
 void global(void);
